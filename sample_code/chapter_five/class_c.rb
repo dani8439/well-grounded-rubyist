51ed5fba7d0f05af7bb693085489c3621e3dd14b
Dani Schuhman <dani8439@aol.com>
2018-04-16 17:24:24 +0000

    notes

--- sample_code/chapter_five/class_c.rb
+++ sample_code/chapter_five/class_c.rb
@@ -69,3 +69,29 @@ end
 C.new.show_var
 
 # I am an instance variable initialized to a string.
+
+
+class C
+  puts "Just inside class definition block. Here's self:"
+  p self
+  @v = "I am an instance variable at the top level of a class body."
+  puts "And here's the instance variable @v, belonging to #{self}:"
+  p @v
+  def show_var
+    puts "Inside an instance method definition block. Here's self:"
+    p self
+    puts "And here's the instance variable @v, belonging to #{self}:"
+    p @v
+  end
+end
+c = C.new
+c.show_var
+
+# Just inside class definition block. Here's self:
+# C
+# And here's the instance variable @v, belonging to C:
+# "I am an instance variable at the top level of a class body."
+# Inside an instance method definition block. Here's self:
+##<C:0x000000021dbd50>
+# And here's the instance variable @v, belonging to #<C:0x000000021dbd50>:
+# nil
