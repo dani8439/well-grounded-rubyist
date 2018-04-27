5a4b1ee40cb59a824d39b2c26987ddcc9b1f67ee
Dani Schuhman <dani8439@aol.com>
2018-04-27 20:19:27 +0000

    x

--- sample_code/chapter_six/x.rb
+++ sample_code/chapter_six/x.rb
@@ -1,13 +1,38 @@
-if false 
-  x = 1
+# The value of statements:
+
+x = 1
+if x < 0
+  puts "negative"
+elsif x > 0
+  puts "positive"
+else
+  puts "zero"
 end
-p x    #<-- Output: nil
-p y    #<-- Fatal error: y is unknown
+
+# positive
+
+# x = 1
+# if x == 2
+#   "it's 2!"
+# elsif x == 3
+#   "it's 3!"
+# end
+
+#entire statement evaluates to nil because it fails (in irb).
+
+# if false
+#   x = 1
+# end
+# p x # output is nil
+# p y # fatal error: y is unknown
+#
+# # x.rb:27:in `<main>': undefined local variable or method `y' for main:Object (NameError)
+#
 
 if x = 1
   puts "Hi!"
 end
 
-# warning: found = in conditional, should be ==
-
-if x = y #<-- No warning. 
+# (irb):2: warning: found = in conditional, should be ==
+# Hi!
+# => nil
