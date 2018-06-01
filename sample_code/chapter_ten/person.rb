a7b826dad269d1556179402b358358345af44f9f
dani8439 <dani8439@aol.com>
2018-06-01 14:34:55 -0400

    Update person.rb

--- sample_code/chapter_ten/person.rb
+++ sample_code/chapter_ten/person.rb
@@ -15,8 +15,8 @@ people = 10.step(25,3).map {|i| Person.new(:age =>i)}
 
 teens = people.partition {|person| person.teenager?}
 
-# => [[], [#<Person:0x000000027512f8 @age=10>, #<Person:0x00000002751280 @age=13>, #<Person:0x00000002750088 @age=16>, #<Person:0x00000002750038 @age=1
-# 9>, #<Person:0x00000002753f08 @age=22>, #<Person:0x00000002743f40 @age=25>]]
+# => [[#<Person:0x0000000101faf8 @age=13>, #<Person:0x0000000101faa8 @age=16>, #<Person:0x0000000101fa58 @age=19>], [#<Person:0x0000000101fb70 @age=10>
+# , #<Person:0x0000000101f9e0 @age=22>, #<Person:0x0000000101f990 @age=25>]]
 
 puts "#{teens[0].size} teens; #{teens[1].size} non-teens"
 # 3 teens; 3 non-teens
