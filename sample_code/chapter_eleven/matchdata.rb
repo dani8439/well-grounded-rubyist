82fbc2745c12bfba79ed2ada16ea3cb840c89974
dani8439 <dani8439@aol.com>
2018-06-15 13:51:54 -0400

    added supplemental code.

--- sample_code/chapter_eleven/matchdata.rb
+++ sample_code/chapter_eleven/matchdata.rb
@@ -25,3 +25,17 @@ puts m[1]                                                 #<----4.
 # Capture #3: 1234
 # Here's another way to get at the first capture:
 # Capture #1: 123
+
+print "The part of the string before the part that matched was: "
+puts m.pre_match
+print "The part of the string after the part that matched was: "
+puts m.post_match
+print "The second capture began at character "
+puts m.begin(2)
+print "The third capture ended at character "
+puts m.end(3)
+
+# The part of the string before the part that matched was: My phone number is
+# The part of the string after the part that matched was: .
+# The second capture began at character 25
+# The third capture ended at character 33
