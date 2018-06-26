fdb234cc2eb575f8ec9229461afea9205c57a690
dani8439 <dani8439@aol.com>
2018-06-26 12:40:50 -0400

    Create decommenter.rb

---
+++ sample_code/chapter_twelve/decommenter.rb
@@ -0,0 +1,14 @@
+module DeCommenter
+  def self.decomment(infile, outfile, comment_re = /\A\s*#/)
+    infile.each do |inline|
+      outfile.print inline unless inline =~ comment_re
+    end
+  end
+end
+
+
+# File.open("myprogram.rb") do |inf|
+#  File.open("myprogram.rb.out", "w") do |outf|
+#    Decommenter.decomment(inf, outf)
+#  end
+# end 
