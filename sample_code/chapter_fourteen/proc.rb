336923af73e300b8ca53ffaca3710c523e0a3b83
dani8439 <dani8439@aol.com>
2018-07-06 15:56:36 -0400

    Update proc.rb

--- sample_code/chapter_fourteen/proc.rb
+++ sample_code/chapter_fourteen/proc.rb
@@ -1,4 +1,17 @@
 pr = Proc.new { puts "Inside a Proc's block" }
-pr.call 
+pr.call
 
 # Inside a Proc's block
+
+def call_a_proc(&block)
+  block.call
+end
+call_a_proc { puts "I'm the block...or Proc...or something." }
+
+# I'm the block...or Proc...or something.
+
+p = Proc.new { |x| puts x.upcase }
+%w{ David Black }.each(&p)
+
+# DAVID
+# BLACK
