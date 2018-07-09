a395b2090b7209fa157451103ca509e4478013f2
dani8439 <dani8439@aol.com>
2018-07-09 11:15:39 -0400

    Create some_proc.rb

---
+++ sample_code/chapter_fourteen/some_proc.rb
@@ -0,0 +1,13 @@
+def call_some_proc(pr)
+  a = "irrelevant 'a' in method scope"
+  puts a
+  pr.call
+end
+a = "'a' to be used in Proc block"
+pr = Proc.new { puts a }
+pr.call
+call_some_proc(pr)
+
+# 'a' to be used in Proc block
+# irrelevant 'a' in method scope
+# 'a' to be used in Proc block
