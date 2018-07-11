58c77a608af7c0bb7104ce942c903e44aecfb442
dani8439 <dani8439@aol.com>
2018-07-11 10:47:59 -0400

    Create thread.rb

---
+++ sample_code/chapter_fourteen/thread.rb
@@ -0,0 +1,21 @@
+# Thread.new do
+#   puts "Starting the thread"
+#   sleep 1
+#   puts "At the end of the thread"
+# end
+# puts "Outside the thread"
+
+# Starting the thread
+# Outside the thread
+
+t = Thread.new do
+  puts "Starting the thread"
+  sleep 1
+  puts "At the end of the thread"
+end
+puts "Outside the thread"
+t.join
+
+# Outside the thread
+# Starting the thread
+# At the end of the thread
