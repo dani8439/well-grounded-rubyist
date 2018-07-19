66322818355a42ef49b79667d5a0830a730cd2b9
dani8439 <dani8439@aol.com>
2018-07-19 14:40:15 -0400

    Create normalize_name.rb

---
+++ sample_code/chapter_fifteen/normalize_name.rb
@@ -0,0 +1,19 @@
+class Person
+  attr_accessor :name
+  def name=(name)
+    @name = name
+    normalize_name
+  end
+  private
+  def normalize_name
+    name.gsub!(/[^-a-z'.\s]/i, "")
+  end
+end
+david = Person.new
+david.name = "123David!! Bl%a9ck"
+raise "Problem" unless david.name == "David Black"
+puts "Name has been normalized."
+p david.private_methods.sort.grep(/normal/)
+
+# Name has been normalized.
+# [:normalize_name]
