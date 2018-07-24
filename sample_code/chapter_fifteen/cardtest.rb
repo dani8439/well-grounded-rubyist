8419c2c07ff2e75cdc4cd9299acb81495df285b0
dani8439 <dani8439@aol.com>
2018-07-24 14:24:09 -0400

    Update cardtest.rb

--- sample_code/chapter_fifteen/cardtest.rb
+++ sample_code/chapter_fifteen/cardtest.rb
@@ -15,26 +15,13 @@ class CardTest < MiniTest::Unit::TestCase
   end
 end
 
-
 # Warning: you should require 'minitest/autorun' instead.
 # Warning: or add 'gem "minitest"' before 'require "minitest/autorun"'
 # From:
 #   cardtest.rb:1:in `<main>'
 # MiniTest::Unit::TestCase is now Minitest::Test. From cardtest.rb:4:in `<main>'
-# Run options: --seed 59230
-# # Running:
-# F
-# Failure:
-# CardTest#test_deal_many [cardtest.rb:14]:
-# Expected: 47
-#   Actual: 43
-#
-# bin/rails test cardtest.rb:12
-# F
-# Failure:
-# CardTest#test_deal_one [cardtest.rb:10]:
-# Expected: 51
-#   Actual: 47
-# bin/rails test cardtest.rb:8
-# Finished in 0.002925s, 683.8767 runs/s, 683.8767 assertions/s.
-# 2 runs, 2 assertions, 2 failures, 0 errors, 0 skips
+# Run options: --seed 54003
+# Running:
+# ..
+# Finished in 0.001792s, 1115.9933 runs/s, 1115.9933 assertions/s.
+# 2 runs, 2 assertions, 0 failures, 0 errors, 0 skips
